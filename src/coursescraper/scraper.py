82f2b7b2ef37352e4120a0e091ea09147da8c3e7
Penacillin <nevinsuri@hotmail.com>
2018-09-22 17:03:35 +1000

    add id and all capacity to json data

--- src/coursescraper/scraper.py
+++ src/coursescraper/scraper.py
@@ -6,11 +6,11 @@ import time
 import re
 
 terms = ["SUMMER TERM", "SEMESTER ONE", "SEMESTER TWO", "SEMESTER THREE"]
-
+utilterms = ["U1", "T1", "T2", "T3"]
 def main():
 
     root = str("http://timetable.unsw.edu.au/" + str(sys.argv[1]) + "/")
-    classutil_url = "http://classutil.unsw.edu.au/{0}_T1.html"
+    classutil_url = "http://classutil.unsw.edu.au/{0}_{1}.html"
     handbookroot = "https://www.handbook.unsw.edu.au/{0}/courses/" + str(sys.argv[1]) + "/"
     soup = bs(get_html(root + "subjectSearch.html"), 'html.parser')
 
@@ -53,7 +53,8 @@ def main():
                                 courseinfo[currarea][course_code]["levels"].append("RESEARCH")
 
                             # load handbook page
-                            coursepage = str(get_html(handbookroot.format("undergraduate" if undergradcourse else "postgraduate") + course_code))
+                            coursepageraw = (get_html(handbookroot.format("undergraduate" if undergradcourse else "postgraduate") + course_code))
+                            coursepage = str(coursepageraw)
                             #prereqs
                             courseinfo[currarea][course_code]["prereqs"] = []
                             if "Prerequisite" in coursepage:
@@ -63,9 +64,19 @@ def main():
                                 if m is not None:
                                     m2 = re.findall(r"\s\w{4}\d{4}", str(m.group(0)))
                                     for prereqMatches in m2:
-                                        print(prereqMatches)
                                         courseinfo[currarea][course_code]["prereqs"].append(str(prereqMatches).strip())
                             
+                            #excluded courses
+                            # handbooksoup = bs(coursepageraw, 'html.parser')
+                            # ex2 = handbooksoup.find(id="exclusion-rules")
+                            # print(ex2)
+                            # excluded = handbooksoup.find_all('div', attrs={"id":"exclusion-rules"})
+                            # print("Excluded:")
+                            # for ex in excluded:
+                            #     pass
+                            #     #print(ex)
+                            # print("End excluded")
+
                             # Units of credits
                             if "6 Units of Credit" in coursepage:
                                 courseinfo[currarea][course_code]["UOC"] = 6
@@ -80,15 +91,19 @@ def main():
 
 
                             # class enrolment
-                            classutilpage = str(get_html(classutil_url.format(course_code[:4])))
-                            m = re.search(r'(name=\"{0}(.+?)(<td>).+?<td>.+?(.+?)(</td><td>).+?(</td><td>).+?(</td><td>)(.+?)(</td>).+?(cu00))'.format(course_code), classutilpage)
-                            if m is not None:
-                                #print(m.group(0))
-                                print(m.group(8))
-                                enrcap = (str(m.group(8))).split("/")
-                                courseinfo[currarea][course_code]["capacity"] = enrcap[1]
+                            courseinfo[currarea][course_code]["capacity"] = []
+                            for i in range(4):
+                                classutilpage = str(get_html(classutil_url.format(course_code[:4], utilterms[i])))
+                                m = re.search(r'(name=\"{0}(.+?)(<td>).+?<td>.+?(.+?)(</td><td>).+?(</td><td>).+?(</td><td>)(.+?)(</td>).+?(cu00))'.format(course_code), classutilpage)
+                                if m is not None:
+                                    #print(m.group(0))
+                                    #print(m.group(8))
+                                    enrcap = (str(m.group(8))).split("/")
+                                    courseinfo[currarea][course_code]["capacity"].append(int(enrcap[1]))
                             
+                            courseinfo[currarea][course_code]["id"] = course_code
                             print(courseinfo[currarea][course_code])
+                            
                             courses += 1
                             if courses > 1 and courses % 10 == 0:
                                 with open("../../data/courseinfo.json", "w")  as f:
